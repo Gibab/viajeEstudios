f61f8eb5b09cdd8f60ab6b941b557fe6a3aa50c6
Raul Rodriguez <raulwrc1@gmail.com>
2017-11-03 10:17:43 +0100

    añadir la vista loging a route

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,25 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+var dsConfig = require('../datasources.local.js');
+var path = require('path');
+
+module.exports = function(app) {
+  // Install a `/` route that returns app login
+  var router = app.loopback.Router();
+
+  //login page
+  app.get('/', function(req, res) {
+    var credentials = dsConfig.myEmailDataSource.transports[0].auth;
+    res.render('login', {
+      email: credentials.user,
+      password: credentials.pass
+    });
+  });
+
+  //verified
+  app.get('/verified', function(req, res) {
+    res.render('verified');
+  });
+  app.use(router);
+
 };
