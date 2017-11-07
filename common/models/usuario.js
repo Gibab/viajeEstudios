a8a734accd793332539539d5979d57cc3aa13eaf
Raul Rodriguez <raulwrc1@gmail.com>
2017-11-07 12:17:15 +0100

    registrarse y verificar el registro

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,43 @@
 'use strict';
+var path =require('path');
 
-module.exports = function(Usuario) {
+module.exports = function (Usuario) {
+    //send verification email after registration
+    Usuario.afterRemote('create', function (context, usuarios, next) {
+        var options = {
+            type: 'email',
+            to: usuarios.email,
+            from: process.env.EMAIL_USER,
+            subject: 'Thanks for registering.',
+            template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+            redirect: '/verified',
+            usuarios: usuarios
+        };
 
+        usuarios.verify(options, function (err, response) {
+            if (err) {
+                Usuario.deleteById(usuarios.id);
+                return next(err);
+            }
+            context.res.render('response', {
+                title: 'Signed up successfully',
+                content: 'Please check your email and click on the verification link ' +
+                'before logging in.',
+                redirectTo: '/',
+                redirectToLinkText: 'Log in'
+            });
+        });
+    });
+
+    // Method to render
+    Usuario.afterRemote('prototype.verify', function (context, usuarios, next) {
+        context.res.render('response', {
+            title: 'A Link to reverify your identity has been sent ' +
+            'to your email successfully',
+            content: 'Please check your email and click on the verification link ' +
+            'before logging in',
+            redirectTo: '/',
+            redirectToLinkText: 'Log in'
+        });
+    });
 };
