0d826c4656ae5efedb68e25fdf034e4548209326
Raul Rodriguez <raulwrc1@gmail.com>
2017-11-08 12:52:08 +0100

    login y cambio de contraseña

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,6 @@
 'use strict';
-var path =require('path');
+var path = require('path');
+var config = require('../../server/config.json');
 
 module.exports = function (Usuario) {
     //send verification email after registration
@@ -36,6 +37,42 @@ module.exports = function (Usuario) {
             'to your email successfully',
             content: 'Please check your email and click on the verification link ' +
             'before logging in',
+            redirectTo: '/',
+            redirectToLinkText: 'Log in'
+        });
+    });
+    //send password reset link when requested
+    Usuario.on('resetPasswordRequest', function (usuarios) {
+        var url = 'http://' + config.host + ':' + config.port + '/reset-password';
+        var html = 'Click <a href="' + url + '?access_token=' +
+            usuarios.accessToken.id + '">here</a> to reset your password';
+
+        Usuario.app.models.Email.send({
+            to: usuarios.email,
+            from: process.env.EMAIL_USER,
+            subject: 'Password reset',
+            html: html
+        }, function (err) {
+            if (err) return console.log('> error sending password reset email');
+            console.log('> sending password reset email to:', usuarios.email);
+        });
+    });
+
+    //render UI page after password change
+    Usuario.afterRemote('changePassword', function (context, usuarios, next) {
+        context.res.render('response', {
+            title: 'Password changed successfully',
+            content: 'Please login again with new password',
+            redirectTo: '/',
+            redirectToLinkText: 'Log in'
+        });
+    });
+
+    //render UI page after password reset
+    Usuario.afterRemote('setPassword', function (context, usuarios, next) {
+        context.res.render('response', {
+            title: 'Password reset success',
+            content: 'Your password has been reset successfully',
             redirectTo: '/',
             redirectToLinkText: 'Log in'
         });
--- server/boot/root.js
+++ server/boot/root.js
@@ -3,12 +3,13 @@
 var dsConfig = require('../datasources.local.js');
 var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Install a `/` route that returns app login
+  var Usuario = app.models.Usuario;
   var router = app.loopback.Router();
 
   //login page
-  app.get('/', function(req, res) {
+  router.get('/', function (req, res) {
     var credentials = dsConfig.myEmailDataSource.transports[0].auth;
     res.render('login', {
       email: credentials.user,
@@ -17,8 +18,75 @@ module.exports = function(app) {
   });
 
   //verified
-  app.get('/verified', function(req, res) {
+  router.get('/verified', function (req, res) {
     res.render('verified');
+  });
+
+  //log a user in
+  router.post('/login', function (req, res) {
+    Usuario.login({
+      email: req.body.email,
+      password: req.body.password
+    }, 'user', function (err, token) {
+      if (err) {
+        if (err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED') {
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/Usuarios/' + err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+      res.render('home', {
+        email: req.body.email,
+        accessToken: token.id,
+        redirectUrl: '/api/Usuarios/change-password?access_token=' + token.id
+      });
+    });
+  });
+
+  //log a user out
+  router.get('/logout', function (req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401);
+    Usuario.logout(req.accessToken.id, function (err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
+  });
+
+  //send an email with instructions to reset an existing user's password
+  router.post('/request-password-reset', function (req, res, next) {    
+    Usuario.resetPassword({
+      email: req.body.email
+    }, function (err) {
+      if (err) return res.status(401).send(err);
+      res.render('response', {
+        title: 'Password reset requested',
+        content: 'Check your email for further instructions',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
+
+  //show password reset form
+  router.get('/reset-password', function (req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401);
+    res.render('password-reset', {
+      redirectUrl: '/api/Usuarios/reset-password?access_token=' +
+      req.accessToken.id
+    });
   });
   app.use(router);
 
